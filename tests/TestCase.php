d3c22159bcf26e373e24c7ca487350e64cd2b1f6
rezaarifandee <rezaarifandee@gmail.com>
2022-04-08 02:01:55 +0700

    Setup test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,28 @@
 
 namespace Tests;
 
+use Faker\Factory as Faker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Artisan;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+	use CreatesApplication, RefreshDatabase;
+	protected $faker;
+
+	public function setUp(): void
+	{
+		parent::setUp();
+		$this->faker = Faker::create();
+		Artisan::call('migrate');
+	}
+
+	public function tearDown(): void
+	{
+		Artisan::call('migrate:rollback');
+
+		parent::tearDown();
+	}
+
 }
