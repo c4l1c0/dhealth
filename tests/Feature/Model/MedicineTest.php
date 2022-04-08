3c33151672eba7beebb2dfc7bd7a970e8e6beb4d
rezaarifandee <rezaarifandee@gmail.com>
2022-04-08 22:34:35 +0700

    Add medicine creation, update, and deletion test

--- tests/Feature/Model/MedicineTest.php
+++ tests/Feature/Model/MedicineTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Feature\Model;
 
 use App\Models\User;
+use App\Models\Medicine;
+use App\Http\Controller\MedicineController;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -29,4 +31,35 @@ class MedicineTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+	public function test_medicine_creation()
+	{
+		$user = User::factory()->create();
+		$this->actingAs($user);
+
+		Medicine::factory()->count(5)->create();
+		$this->assertDatabaseCount('medicines', 5);
+	}
+
+	public function test_medicine_update()
+	{
+		$user = User::factory()->create();
+		$this->actingAs($user);
+
+		$medicine = Medicine::factory()->create();
+		$medicine->update(['name' => 'UPDATED']);
+		$this->assertDatabaseHas('medicines', [
+			'name' => 'UPDATED'
+		]);
+	}
+
+	public function test_medicine_deletion()
+	{
+		$user = User::factory()->create();
+		$this->actingAs($user);
+
+		$medicine = Medicine::factory()->create();
+		$medicine->delete();
+		$this->assertSoftDeleted($medicine);
+	}
 }
