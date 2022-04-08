a4530cfddf38a3b7b95308974a4107709e865ae9
rezaarifandee <rezaarifandee@gmail.com>
2022-04-08 22:40:41 +0700

    Add every model controllers

---
+++ app/Http/Controllers/RecipeController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RecipeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+		$medicines = Recipe::with('createdBy')->paginate(20);
+		return view('recipes.index', ['recipe' => $recipe]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+		return view('recipes.form');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+		$this->validate($request, [
+			'name' => 'required',
+		]);
+
+		$db = Recipe::create($request->all());
+		Session::flash("status", [
+			"level"=>"success",
+			"message"=>"Berhasil Di Simpan"
+		]);
+
+		return redirect()->route('recipes.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+		$data = Recipe::find($id);
+
+		return view('recipes.form')->with(compact('data'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+		$this->validate($request, [
+			'name' => 'required',
+		]);
+
+		$db = Recipe::find($id);
+		$db->update($request->all());
+		Session::flash("status", [
+			"level"=>"success",
+			"message"=>"Saved successfully"
+		]);
+
+		return redirect()->route('recipes.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+		Recipe::find($id)->delete();
+		return redirect()->route('recipes.index');
+    }
+}
